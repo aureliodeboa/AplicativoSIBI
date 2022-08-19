034c0ddad143483216b6adc4172d502de4b811d0
Attanz <Jonathan_lucas.s@hotmail.com>
2022-08-19 10:47:14 -0300

    adiciona-tela-guiausuario
    implementei a função para abrir tela guia do usuário.

---
+++ lib/telaguiausuario.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:sibi/telabiblioteca.dart';
+
+class telaguiausuario extends StatefulWidget {
+  const telaguiausuario({Key? key}) : super(key: key);
+  @override
+  State<telaguiausuario> createState() => _telaguiausuarioState();
+}
+
+class _telaguiausuarioState extends State<telaguiausuario> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
