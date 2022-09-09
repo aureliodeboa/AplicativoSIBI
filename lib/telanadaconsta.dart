5931a904a3dde6d0cf9fd02f3902ffd8ce2c78d1
Gabriel Bernardo <gabriel.bernardo@sysvale.com>
2022-09-09 08:36:49 -0300

    feat: atualiza diagramação das subtelas do menu

--- lib/telanadaconsta.dart
+++ lib/telanadaconsta.dart
@@ -13,55 +13,86 @@ class telanadaconsta extends StatefulWidget {
 
 class _telanadaconstaState extends State<telanadaconsta> {
   void launchExternalWebsite(String url) async {
-    var urlUri= Uri.parse(url);
-    if(await canLaunchUrl(urlUri)){
+    var urlUri = Uri.parse(url);
+    if (await canLaunchUrl(urlUri)) {
       await launchUrl(urlUri, mode: LaunchMode.externalApplication);
-    }else {
+    } else {
       throw 'could not lanch $urlUri';
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.blue, title: Text("Declaração Nada Consta")),
+      appBar: AppBar(
+          backgroundColor: Colors.blue, title: Text("Declaração Nada Consta")),
       body: SingleChildScrollView(
         padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
         child: Center(
-
           child: Column(
             children: [
-              Text("\n Para que o discente conclua a graduação  na UNIVASF e receba o tão esperado diploma, é necessário emitir a declaração de isenção de débito “ nada consta “ "
-                  "a seguir é demonstrado o passo a passo para emitir: \n "),
-              Text("\n• Primeiro deve-se acessar o Sistema Pergamum, disponível no link abaixo ou pelo Portal SIBI"),
-              Text("\n• Logar-se com seu CPF e a senha da biblioteca, as mesmas credenciais usadas para pegar um livro emprestado na biblioteca \n"
-                  "\n• Ir na aba empréstimos e clicar na aba Declaração nada Consta como mostra a imagem a seguir: \n"),
-              Image.asset('imagem/nadaconsta.jpg',width: 350,height: 300, ),
-              Text("\n•Logo após será aberto uma  notificação para confirmar, como mostra a próxima imagem:"),
-              Image.asset('imagem/nadaconsta2.png',width: 350,height: 300),
-              Text("\n• Logo após será emitido o “ nada consta ”                           \n"),
-              Text("\n Caso não tenha ficado claro  veja o tutorial a seguir :        \n"),
-              YoutubePlayer(controller: YoutubePlayerController(
-                  initialVideoId: 'iLDxNersLDQ',
-                  flags: YoutubePlayerFlags(
-                    autoPlay: false,
-                    mute:  false,
-                    showLiveFullscreenButton: true,
-                    hideControls: false,
-                  )),),
+              Text(
+                  "\n Para que o discente conclua a graduação  na UNIVASF e receba o tão esperado diploma, é necessário emitir a declaração de isenção de débito “ nada consta “ "
+                  "a seguir é demonstrado o passo a passo para emitir: \n ",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+              Text(
+                  "\n• Primeiro deve-se acessar o Sistema Pergamum, disponível no link abaixo ou pelo Portal SIBI",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+              Text(
+                  "\n• Logar-se com seu CPF e a senha da biblioteca, as mesmas credenciais usadas para pegar um livro emprestado na biblioteca \n"
+                  "\n• Ir na aba empréstimos e clicar na aba Declaração nada Consta como mostra a imagem a seguir: \n",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+              Image.asset(
+                'imagem/nadaconsta.jpg',
+                width: 350,
+                height: 300,
+              ),
+              Text(
+                  "\n•Logo após será aberto uma  notificação para confirmar, como mostra a próxima imagem:",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+              Image.asset('imagem/nadaconsta2.png', width: 350, height: 300),
+              Text(
+                  "\n• Logo após será emitido o “ nada consta ”                           \n",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+              Text(
+                  "\n Caso não tenha ficado claro  veja o tutorial a seguir :        \n",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+              YoutubePlayer(
+                controller: YoutubePlayerController(
+                    initialVideoId: 'iLDxNersLDQ',
+                    flags: YoutubePlayerFlags(
+                      autoPlay: false,
+                      mute: false,
+                      showLiveFullscreenButton: true,
+                      hideControls: false,
+                    )),
+              ),
               TextButton.icon(
-                icon: Container(child: Image.asset('imagem/icones/pergamun.png',width: 25 ,color:  Colors.amber[700],),),
-                label: Text(' Acesse o pergamum clicando aqui',style: TextStyle(color: Colors.amber[700])),
+                icon: Container(
+                  child: Image.asset(
+                    'imagem/icones/pergamun.png',
+                    width: 25,
+                    color: Color.fromARGB(255, 253, 152, 2),
+                  ),
+                ),
+                label: Text(
+                        ' Acesse o pergamum clicando aqui',
+                        style: 
+                          TextStyle(
+                            color: Color.fromARGB(255, 253, 152, 2),
+                            fontSize: 18,
+                          )
+                      ),
+                
                 onPressed: () => setState(() {
-                  launchExternalWebsite("https://biblioteca.univasf.edu.br/pergamum/biblioteca_s/php/login_usu.php?flag=index.php");
-
-                }
-                ),),
-
+                  launchExternalWebsite(
+                      "https://biblioteca.univasf.edu.br/pergamum/biblioteca_s/php/login_usu.php?flag=index.php");
+                }),
+              ),
             ],
-
           ),
         ),
-      ) ,
+      ),
     );
   }
 }
--- lib/telatutoriais.dart
+++ lib/telatutoriais.dart
@@ -12,73 +12,105 @@ class _telatutoriaisState extends State<telatutoriais> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue[400],title: Text('Tutoriais')
-
-      ),
+      appBar:
+          AppBar(backgroundColor: Colors.blue[400], title: Text('Tutoriais')),
       body: SingleChildScrollView(
         child: Center(
-
-          child:Container(
-            padding:  EdgeInsets.fromLTRB(20, 20, 20, 20),
-            child:  Column(
+          child: Container(
+            padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+            child: Column(
               children: [
-
-                Text('Emissão da declaração de isenção de débito "nada consta"\n ',style: TextStyle(color: Colors.amber[700], fontSize: 20,)),
+                Text(
+                    'Emissão da declaração de isenção de débito "nada consta"\n ',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 253, 152, 2),
+                      fontSize: 25,
+                      fontWeight: FontWeight.w700
+                    )),
                 //Text('O video a seguir mostra como conseguir o "nada consta" :',style: TextStyle(color: Colors.black, fontSize: 18,)),
 
-                YoutubePlayer(controller: YoutubePlayerController(
-                    initialVideoId: 'iLDxNersLDQ',
-                    flags: YoutubePlayerFlags(
-                      autoPlay: false,
-                      mute:  false,
-                      showLiveFullscreenButton: true,
-                      hideControls: false,
+                YoutubePlayer(
+                  controller: YoutubePlayerController(
+                      initialVideoId: 'iLDxNersLDQ',
+                      flags: YoutubePlayerFlags(
+                        autoPlay: false,
+                        mute: false,
+                        showLiveFullscreenButton: true,
+                        hideControls: false,
+                      )),
+                ),
+                SizedBox(
+                  width: 30,
+                  height: 30,
+                ),
 
-                    )),),
-                SizedBox(width: 30,height: 30,),
+                Text(
+                    'Como fazer a renovação de um livro da biblioteca on-line? \n',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 253, 152, 2),
+                      fontSize: 25,
+                      fontWeight: FontWeight.w700
+                    )),
 
-                Text('Como fazer a renovação de um livro da biblioteca on-line? \n',style: TextStyle(color: Colors.amber[700], fontSize: 20,)),
+                YoutubePlayer(
+                  controller: YoutubePlayerController(
+                      initialVideoId: 'WsF1_gYREXc',
+                      flags: YoutubePlayerFlags(
+                        autoPlay: false,
+                        mute: false,
+                        showLiveFullscreenButton: true,
+                        hideControls: false,
+                      )),
+                ),
+                SizedBox(
+                  width: 30,
+                  height: 30,
+                ),
 
-                YoutubePlayer(controller: YoutubePlayerController(
-                    initialVideoId: 'WsF1_gYREXc',
-                    flags: YoutubePlayerFlags(
-                      autoPlay: false,
-                      mute:  false,
-                      showLiveFullscreenButton: true,
-                      hideControls: false,
-                    )),),
-                SizedBox(width: 30,height: 30,),
-
-                Text('Como ter acesso as normas ABNT pelo catalgo Pergamun? \n',style: TextStyle(color: Colors.amber[700], fontSize: 20,)),
+                Text('Como ter acesso as normas ABNT pelo catalgo Pergamun? \n',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 253, 152, 2),
+                      fontSize: 25,
+                      fontWeight: FontWeight.w700
+                    )),
                 //Text('Siga as instruções do video asseguir:',style: TextStyle(color: Colors.black, fontSize: 18,)),
-                YoutubePlayer(controller: YoutubePlayerController(
-                  initialVideoId: 'ix07V_Xfc1E',
-
-                  flags: YoutubePlayerFlags(
-                    autoPlay: false,
-                    mute:  false,
-                    showLiveFullscreenButton: true,
-                    hideControls: false,
-
-                  ),
-                ),),
-                SizedBox(width: 30,height: 30,),
-
-                Text('Como fazer Consultas no Pergamum? \n',style: TextStyle(color: Colors.amber[700], fontSize: 20,)),
-               // Text('Siga as instruções do video asseguir:',style: TextStyle(color: Colors.black, fontSize: 18,)),
-                YoutubePlayer(controller: YoutubePlayerController(
-                    initialVideoId: 'cTIByTdQQh0',
+                YoutubePlayer(
+                  controller: YoutubePlayerController(
+                    initialVideoId: 'ix07V_Xfc1E',
                     flags: YoutubePlayerFlags(
                       autoPlay: false,
-                      mute:  false,
+                      mute: false,
                       showLiveFullscreenButton: true,
                       hideControls: false,
-                    )),),
-                SizedBox(width: 30,height: 30,),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 30,
+                  height: 30,
+                ),
 
-
-
+                Text('Como fazer Consultas no Pergamum? \n',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 253, 152, 2),
+                      fontSize: 25,
+                      fontWeight: FontWeight.w700
+                    )),
+                // Text('Siga as instruções do video asseguir:',style: TextStyle(color: Colors.black, fontSize: 18,)),
+                YoutubePlayer(
+                  controller: YoutubePlayerController(
+                      initialVideoId: 'cTIByTdQQh0',
+                      flags: YoutubePlayerFlags(
+                        autoPlay: false,
+                        mute: false,
+                        showLiveFullscreenButton: true,
+                        hideControls: false,
+                      )),
+                ),
+                SizedBox(
+                  width: 30,
+                  height: 30,
+                ),
               ],
             ),
           ),
